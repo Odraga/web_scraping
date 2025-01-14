5b957cfd65cce184963880b860c9da2d6c7d0bbe
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2025-01-14 17:11:12 -0400

    Update

--- src/api/API.js
+++ src/api/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Instancia de Axios
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3000/api/v1/",
+  baseURL: import.meta.env.VITE_API_BASE,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
