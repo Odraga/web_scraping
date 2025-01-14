da865f2d71caadcbfc50031a65a5742f1c0ff5bc
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2025-01-14 16:46:24 -0400

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import MostVisitedURLs from "../../components/MostVisitedURLs/MostVisitedURLs";
-/* import URLs from "../../components/URLsCreate/URLsCreate";
- */
+import URLs from "../../components/URLsCreate/URLsCreate";
+
 const Home = () => {
   const [anyChange, setAnyChange] = useState(false);
 
@@ -18,7 +18,7 @@ const Home = () => {
           <h1 className=" fw-bolder text-primary text-shadow text-center my-5">
             Short URL
           </h1>{" "}
-          {/*  <URLs makeAnyChange={makeAnyChange} /> */}
+          <URLs makeAnyChange={makeAnyChange} />
           <MostVisitedURLs
             makeAnyChange={makeAnyChange}
             anyChange={anyChange}
